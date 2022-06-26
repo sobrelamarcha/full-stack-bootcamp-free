cc57fb3d8fb55ccd89db0d47eb3b600e3af5e747
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-06-26 20:43:31 +0200

    fix de error sugerido por React para la versión 18

--- part1/ej1/src/index.js
+++ part1/ej1/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const Header = (props) => {
   return <h1>{props.course}</h1>;
@@ -59,4 +59,6 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
