b3dcef1797857ff84df00796de95837b9181d0ee
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-06-26 15:27:21 +0200

    paso 1: contadores

--- part1/ej2/src/index.js
+++ part1/ej2/src/index.js
@@ -7,7 +7,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleBtnGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleBtnNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBtnBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={handleBtnGood}>good</button>
+      <button onClick={handleBtnNeutral}>neutral</button>
+      <button onClick={handleBtnBad}>bad</button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
