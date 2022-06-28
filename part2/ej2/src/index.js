50e9ffa80ed3a8b03db25cf4db12abe2bbe07373
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-06-28 13:44:53 +0200

    parte2 ej2 paso 3: Añadir teléfono y filtro de búsqueda por nombre

--- part2/ej2/src/index.js
+++ part2/ej2/src/index.js
@@ -1,36 +1,83 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-const Numbers = ({ persons }) => {
-  const personsList = persons.map((person) => {
-    return <p key={person.id}>{person.name}</p>;
-  });
+const Numbers = ({ persons, filter }) => {
+  const personsList = persons
 
-  return <>{personsList}</>;
+    .filter((person) => {
+      return person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+    })
+
+    .map((person) => {
+      return (
+        <tr key={person.id}>
+          <td>{person.name}</td>
+          <td>{person.phone}</td>
+        </tr>
+      );
+    });
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>{personsList}</tbody>
+      </table>
+    </>
+  );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas", phone: "111" },
+    { id: 2, name: "Pepe Domingo", phone: "222" },
+    { id: 3, name: "Juan Pedro", phone: "333" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
 
     // comprobar si existe
-    const existe = persons.find((o) => o.name === newName);
+    const existe = persons.find((person) => person.name === newName);
     if (existe) {
       alert(`${newName} ya existe, no voy a añadirlo`);
       return;
     }
 
-    // añadir nombre
-    const nuevoObj = { id: persons.length + 1, name: newName };
+    // comprobar que el name no está vacío
+    if (newName.trim() === "") {
+      alert("Debes escribir algún nombre válido");
+      return;
+    }
+
+    // añadir nombre y teléfono
+    const nuevoObj = { id: persons.length + 1, name: newName, phone: newPhone };
     setPersons(persons.concat(nuevoObj));
     console.log("añadido");
   };
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+    const namesFiltered = persons.filter((p) => {
+      return p.name.indexOf(newFilter) !== -1;
+    });
+    console.log(namesFiltered);
   };
 
   return (
@@ -42,11 +89,20 @@ const App = () => {
           Name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
+          Phone: <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <div>
+        Filter shown with:{" "}
+        <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <div>
+        <Numbers persons={persons} filter={newFilter} />
+      </div>
     </div>
   );
 };
