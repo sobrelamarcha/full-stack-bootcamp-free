2194d762d8bbeec87b529e75548b00d8795ca5c5
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-06-29 12:37:53 +0200

    part2 ej3: clic sobre pais

--- part2/ej3/src/index.js
+++ part2/ej3/src/index.js
@@ -1,36 +1,85 @@
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
-const Countries = ({ countries, filter }) => {
+const CountryDetails = ({ selectedCountry, countryLanguages }) => {
+  return (
+    <div>
+      <p>Name: {selectedCountry.name}</p>
+      <p>Capital: {selectedCountry.capital}</p>
+      <p>Population: {selectedCountry.population}</p>
+      <h3>Languages:</h3>
+      <ul>{countryLanguages}</ul>
+      <h3>Flag:</h3>
+      <p>
+        <img alt="bandera" src={selectedCountry.flags.png} />
+      </p>
+    </div>
+  );
+};
+
+const CountryDetailsClicked = ({ countryToShow, filteredCountries }) => {
+  const selectedCountry = filteredCountries.find((c) => {
+    return c.name === countryToShow;
+  });
+  const countryLanguages = selectedCountry.languages.map((c) => {
+    return <li key={c.name}>{c.name}</li>;
+  });
+
+  return (
+    <CountryDetails
+      selectedCountry={selectedCountry}
+      countryLanguages={countryLanguages}
+    />
+  );
+};
+
+const CountryDetailsOneFiltered = ({ filteredCountries }) => {
+  const selectedCountry = filteredCountries[0];
+  const countryLanguages = selectedCountry.languages.map((c) => {
+    return <li key={c.name}>{c.name}</li>;
+  });
+
+  return (
+    <CountryDetails
+      selectedCountry={selectedCountry}
+      countryLanguages={countryLanguages}
+    />
+  );
+};
+
+const Countries = ({
+  countries,
+  filter,
+  handleCountryButton,
+  countryToShow,
+}) => {
   const filteredCountries = countries.filter((c) => {
     return c.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
   });
   const countriesList = filteredCountries.map((c) => {
-    return <li key={c.name}>{c.name}</li>;
+    return (
+      <li key={c.name}>
+        {c.name}
+        <button onClick={() => handleCountryButton(c.name)}>show</button>
+      </li>
+    );
   });
+
+  if (countryToShow.trim() !== "") {
+    return (
+      <CountryDetailsClicked
+        countryToShow={countryToShow}
+        filteredCountries={filteredCountries}
+      />
+    );
+  }
 
   if (countriesList.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
 
   if (filteredCountries.length === 1) {
-    const selectedCountry = filteredCountries[0];
-    const countryLanguages = selectedCountry.languages.map((c) => {
-      return <li key={c.name}>{c.name}</li>;
-    });
-    return (
-      <div>
-        <p>Name: {selectedCountry.name}</p>
-        <p>Capital: {selectedCountry.capital}</p>
-        <p>Population: {selectedCountry.population}</p>
-        <h3>Languages:</h3>
-        <ul>{countryLanguages}</ul>
-        <h3>Flag:</h3>
-        <p>
-          <img alt="bandera" src={selectedCountry.flags.png} />
-        </p>
-      </div>
-    );
+    return <CountryDetailsOneFiltered filteredCountries={filteredCountries} />;
   }
 
   return (
@@ -56,12 +105,20 @@ const App = () => {
       });
   }, []);
 
+  const [countryToShow, setCountryToShow] = useState("");
   const [countries, setCountries] = useState(countriesData);
   const [newFilter, setNewFilter] = useState("");
 
   const handleFilterChange = (event) => {
     // console.log(event.target.value);
     setNewFilter(event.target.value);
+    // limpiamos por si hemos hecho clic en un pais
+    setCountryToShow("");
+  };
+
+  const handleCountryButton = (country) => {
+    // console.log(country);
+    setCountryToShow(country);
   };
 
   return (
@@ -74,7 +131,12 @@ const App = () => {
       </div>
 
       {/* <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} /> */}
-      <Countries countries={countries} filter={newFilter} />
+      <Countries
+        countries={countries}
+        filter={newFilter}
+        handleCountryButton={handleCountryButton}
+        countryToShow={countryToShow}
+      />
     </div>
   );
 };
