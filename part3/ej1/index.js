7114ac9c629d5f17003739a54b178e78f2a8f7c3
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-02 17:43:46 +0200

    mostrar info

--- part3/ej1/index.js
+++ part3/ej1/index.js
@@ -28,8 +28,27 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const totalPersons = persons.length;
+  const fechaHoy = new Date();
+  response.send(
+    `Phonebook has info for ${totalPersons} people <br>${fechaHoy}`
+  );
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const idPerson = Number(request.params.id);
+  const person = persons.find((p) => p.id === idPerson);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
