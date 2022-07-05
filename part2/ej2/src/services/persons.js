a603d542c9841d816950a517c45612e086ea4709
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-05 18:02:24 +0200

    Preparar para trabajo con heroku

--- part2/ej2/src/services/persons.js
+++ part2/ej2/src/services/persons.js
@@ -1,5 +1,6 @@
 //const baseUrl = "http://localhost:3001/persons"; // enviar datos por POST al servidor json-server
-const baseUrl = "http://localhost:3001/api/persons"; // enviar datos por POST a la api creada en NodeJS de la part3 ej1
+// const baseUrl = "http://localhost:3001/api/persons"; // enviar datos por POST a la api creada en NodeJS de la part3 ej1
+const baseUrl = "https://tranquil-congaree-79881.herokuapp.com/api/persons"; // enviar datos por POST a la api subida a Heroku
 
 export const getAllPersons = () => {
   return fetch(baseUrl);
