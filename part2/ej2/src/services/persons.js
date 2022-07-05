670a37a97599248e97986bd3dfd0de1686c6cb3d
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-05 18:46:06 +0200

    Path relativo porque subimos el cliente también a heroku. Para que también funcione en desarrollo usamos proxy en package.json

--- part2/ej2/src/services/persons.js
+++ part2/ej2/src/services/persons.js
@@ -1,6 +1,7 @@
 //const baseUrl = "http://localhost:3001/persons"; // enviar datos por POST al servidor json-server
 // const baseUrl = "http://localhost:3001/api/persons"; // enviar datos por POST a la api creada en NodeJS de la part3 ej1
-const baseUrl = "https://tranquil-congaree-79881.herokuapp.com/api/persons"; // enviar datos por POST a la api subida a Heroku
+// const baseUrl = "https://tranquil-congaree-79881.herokuapp.com/api/persons"; // enviar datos por POST a la api subida a Heroku
+const baseUrl = "/api/persons"; // Debido a que este cliente se sube a heroku junto con la parte servidora, se especifica la ruta como relativa para llamar a la parte servidora
 
 export const getAllPersons = () => {
   return fetch(baseUrl);
