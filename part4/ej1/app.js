c0c50b7ba9ee4170a0551e5965d5b02662636414
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-12 20:30:20 +0200

    comenzar app de listado de blogs (parte 4 ej1)

---
+++ part4/ej1/app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+const mongoUrl = config.MONGODB_URI
+logger.info(`connecting to ${mongoUrl}`)
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
