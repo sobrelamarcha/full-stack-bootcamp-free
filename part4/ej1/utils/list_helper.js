120c7e635d7d88e8e1df8c21b8594769e509510c
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-16 11:43:30 +0200

    totalLikes y favoriteBlog

--- part4/ej1/utils/list_helper.js
+++ part4/ej1/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(function (acc, obj) { return acc + obj.likes }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  // Find the maximum number of votes first
+
+  const listLikes = blogs.map(object => {
+    return object.likes
+  })
+
+  const maxLikes = Math.max(...listLikes)
+
+  // Find the blog that contains the number of likes obtained in the previous step
+  return blogs.find((blog) => blog.likes === maxLikes)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
