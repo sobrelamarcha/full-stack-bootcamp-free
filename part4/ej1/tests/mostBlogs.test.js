c6fb4c970ade67c4b52188c61d732ab673f5c0a7
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-22 20:21:30 +0200

    Fix mostBlogs to return the object specified by the exercise

--- part4/ej1/tests/mostBlogs.test.js
+++ part4/ej1/tests/mostBlogs.test.js
@@ -1,8 +1,14 @@
 const listHelper = require('../utils/list_helper')
 const dataForTests = require('../utils/data_for_tests')
 describe('Author who has more blogs', () => {
-  test('when list has many blogs', () => {
+  test.only('when list has many blogs', () => {
+    const expectedResult = {
+      author: 'Robert C. Martin',
+      blogs: 3
+    }
+
     const result = listHelper.mostBlogs(dataForTests.listWithManyBlogs)
-    expect(result).toBe('Robert C. Martin')
+
+    expect(result).toStrictEqual(expectedResult)
   })
 })
--- part4/ej1/utils/list_helper.js
+++ part4/ej1/utils/list_helper.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+// const testdata = require('./data_for_tests')
 
 const dummy = (blogs) => {
   return 1
@@ -24,10 +25,18 @@ const favoriteBlog = (blogs) => {
 const mostBlogs = (blogs) => {
   const contadores = _.countBy(blogs, 'author') //=
   const keys = Object.keys(contadores) //=
-  const maximo = _.maxBy(keys, (e) => { return contadores[e] }) //=
+  const maximo = _.maxBy(keys, (elem) => { return contadores[elem] }) //=
 
-  return maximo
+  const finalAuthor = {
+    author: maximo,
+    blogs: contadores[maximo]
+  }
+  // console.log(finalAuthor)
+
+  return finalAuthor
 }
+
+// const result = mostBlogs(testdata.listWithManyBlogs) //=
 
 module.exports = {
   dummy, totalLikes, favoriteBlog, mostBlogs
